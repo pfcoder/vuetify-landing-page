d91aaba443fa2be43620336081c2d66a5748e10b
pfcoder <pfcoder97@gmail.com>
2022-03-02 15:48:17 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import VueYouTubeEmbed from 'vue-youtube-embed'
+//import VueYouTubeEmbed from 'vue-youtube-embed'
 // import firebase from 'firebase/app'
 // import 'firebase/firestore'
 
-Vue.use(VueYouTubeEmbed)
+//Vue.use(VueYouTubeEmbed)
 Vue.config.productionTip = false
 
 // firebase.initializeApp({
